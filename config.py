0cb19710414f8b82a40595dc5770eb7a5f9716ad
rsrawatofficial <rsrasasingh@gmail.com>
2025-05-06 07:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
     API_ID = int(os.environ.get("API_ID", "27900743"))
-    API_HASH = os.environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
+    API_HASH = os.environ.get("API_HASH", "7857589413:AAE79aTmwa6-LIdbK0L4xS1byZJQIH2IR8U")
     AUTH_USER = os.environ.get("AUTH_USERS", '7804396225').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
